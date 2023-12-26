045b766703f578295600f82ee56bd81dd013cfa7
Lucas <lucasbombaron@gmail.com>
2023-12-26 16:37:05 +0100

    hero details

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -7,15 +7,15 @@ function Hero() {
     return(
         <section className="hero">
             <div className="hero__verso">
-                <p className="hero__verso__security">123</p>
+                <p className="hero__verso__security">000</p>
                 <img src={versoCard} alt='verso' className="hero__verso__img"/>
             </div>
             <div className="hero__recto">
                 <img src={cardLogo} alt="card logo" className="hero__recto__cardLogo"/>
-                <p className="hero__recto__number">9591 6489 6389 1011</p>
+                <p className="hero__recto__number">0000 0000 0000 0000</p>
                 <div className="hero__recto__infos">
-                    <p className="hero__recto__infos__name">Felicia Leire</p>
-                    <p className="hero__recto__infos__expiration">09/26</p>
+                    <p className="hero__recto__infos__name">Jane Appleseed</p>
+                    <p className="hero__recto__infos__expiration">00/00</p>
                 </div>
                 <img src={rectoCard} alt='recto' className="hero__recto__img"/>
             </div>
