968b3106890e9eff0276b2744d47cccfcc754dd1
Lucas <lucasbombaron@gmail.com>
2023-12-29 09:45:55 +0100

    form v2

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -8,26 +8,26 @@ function Form() {
     console.log(errors);
 
     return(
-        <section className="form">
-            <form onSubmit={handleSubmit(onSubmit)}>
+        <section className="form__container">
+            <form onSubmit={handleSubmit(onSubmit)} class>
                 <div className='form__input'>
                     <p className='form__input__title'>Cardholder Name</p>
-                    <input className='form__input__cname input' type="text" placeholder="e.g. Jane Appleseed" {...register} />
+                    <input className='form__input__cname largeInput' type="text" placeholder="e.g. Jane Appleseed" {...register} />
                 </div>
                 <div className='form__input'>
                     <p className='form__input__title'>Card Number</p>
-                    <input className='form__input__cnumber input' type="number" placeholder="e.g. 1234 5678 9123 0000" {...register("Card Number", {})} />
+                    <input className='form__input__cnumber largeInput' type="number" placeholder="e.g. 1234 5678 9123 0000" {...register("Card Number", {})} />
                 </div>
-                <div className='form__input'>
+                <div className='form__input__bottom'>
                     <p className='form__input__title'>Exp. Date (MM/YY)</p>
                     <div className='form__input__cexpiration--container'>
-                        <input className='form__input__cexpiration input' type="number" placeholder="MM" {...register("Expiration date", {})} />
-                        <input className='form__input__cexpiration input' type="number" placeholder="YY" {...register("Expiration date", {})} />
+                        <input className='form__input__cexpiration smallInput' type="number" placeholder="MM" {...register("Expiration date", {})} />
+                        <input className='form__input__cexpiration smallInput' type="number" placeholder="YY" {...register("Expiration date", {})} />
                     </div>
                 </div>
-                <div className='form__input'>
+                <div className='form__input__bottom'>
                     <p className='form__input__title'>CVC</p>
-                    <input className='form__input__cvc input' type="number" placeholder="e.g. 123" {...register("CVC", {})} />
+                    <input className='form__input__cvc smallInput' type="number" placeholder="e.g. 123" {...register("CVC", {})} />
                 </div>
         
                 <input type="submit" className='form__submit'/>
