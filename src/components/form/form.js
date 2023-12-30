44117b5180e8484c3b3768f4dcb20aaae6ff3bc1
Lucas <lucasbombaron@gmail.com>
2023-12-30 10:27:32 +0100

    form final

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -9,28 +9,28 @@ function Form() {
 
     return(
         <section className="form__container">
-            <form onSubmit={handleSubmit(onSubmit)} class>
-                <div className='form__input'>
+            <form onSubmit={handleSubmit(onSubmit)} className='form'>
+                <div className='form__input1'>
                     <p className='form__input__title'>Cardholder Name</p>
                     <input className='form__input__cname largeInput' type="text" placeholder="e.g. Jane Appleseed" {...register} />
                 </div>
-                <div className='form__input'>
+                <div className='form__input2'>
                     <p className='form__input__title'>Card Number</p>
                     <input className='form__input__cnumber largeInput' type="number" placeholder="e.g. 1234 5678 9123 0000" {...register("Card Number", {})} />
                 </div>
-                <div className='form__input__bottom'>
+                <div className='form__input3'>
                     <p className='form__input__title'>Exp. Date (MM/YY)</p>
                     <div className='form__input__cexpiration--container'>
                         <input className='form__input__cexpiration smallInput' type="number" placeholder="MM" {...register("Expiration date", {})} />
                         <input className='form__input__cexpiration smallInput' type="number" placeholder="YY" {...register("Expiration date", {})} />
                     </div>
                 </div>
-                <div className='form__input__bottom'>
+                <div className='form__input4'>
                     <p className='form__input__title'>CVC</p>
                     <input className='form__input__cvc smallInput' type="number" placeholder="e.g. 123" {...register("CVC", {})} />
                 </div>
         
-                <input type="submit" className='form__submit'/>
+                <input type="submit" className='form__submit' value='Confirm'/>
             </form>
         </section>
     )
