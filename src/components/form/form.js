07d13b6a29c97765256485840ca5311bae6f6879
Lucas <lucasbombaron@gmail.com>
2024-01-01 18:35:18 +0100

    input patterns

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -12,7 +12,7 @@ function Form() {
         setVal(e.target.value);
       };
 
-    function cc_format(value) {
+    function cn_format(value) {
         const v = value
           .replace(/\s+/g, "")
           .replace(/[^0-9]/gi, "")
@@ -32,7 +32,12 @@ function Form() {
             <form onSubmit={handleSubmit(onSubmit)} className='form'>
                 <div className='form__input1'>
                     <p className='form__input__title'>Cardholder Name</p>
-                    <input className='form__input__cname largeInput' type="text" placeholder="e.g. Jane Appleseed" />
+                    <input 
+                    className='form__input__cname largeInput' 
+                    type="text" 
+                    placeholder="e.g. Jane Appleseed" 
+                    required
+                    pattern='[a-z A-Z-]+'/>
                 </div>
                 <div className='form__input2'>
                     <p className='form__input__title'>Card Number</p>
@@ -40,20 +45,35 @@ function Form() {
                     className='form__input__cnumber largeInput' 
                     type="tel" 
                     placeholder="e.g. 1234 5678 9123 0000" 
-                    value={cc_format(val)}
+                    value={cn_format(val)}
                     onChange={onChange}
-                    />
+                    required />
                 </div>
                 <div className='form__input3'>
                     <p className='form__input__title'>Exp. Date (MM/YY)</p>
                     <div className='form__input__cexpiration--container'>
-                        <input className='form__input__cexpiration smallInput' type="number" placeholder="MM" />
-                        <input className='form__input__cexpiration smallInput' type="number" placeholder="YY" />
+                        <input 
+                        className='form__input__cexpiration smallInput' 
+                        type="tel" 
+                        placeholder="MM"
+                        pattern='\d\d'
+                        required />
+                        <input 
+                        className='form__input__cexpiration smallInput' 
+                        type="tel" 
+                        placeholder="YY"
+                        pattern='\d\d'
+                        required />   
                     </div>
                 </div>
                 <div className='form__input4'>
                     <p className='form__input__title'>CVC</p>
-                    <input className='form__input__cvc smallInput' type="number" placeholder="e.g. 123" />
+                    <input 
+                    className='form__input__cvc smallInput' 
+                    type="tel" 
+                    placeholder="e.g. 123" 
+                    pattern="\d{3}"
+                    required />
                 </div>
         
                 <input type="submit" className='form__submit' value='Confirm'/>
