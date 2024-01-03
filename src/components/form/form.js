a3f3d828cd92bfd45a98325eb66972f3117ff25b
Lucas <lucasbombaron@gmail.com>
2024-01-03 15:34:11 +0100

    mise à jour des composants grâce à redux et update formatage mois

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,16 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { setFormValue } from '../../redux/action';
 
 function Form() {
 
-    const { handleSubmit, formState: { errors } } = useForm();
+    const dispatch = useDispatch();
+
+    const { handleSubmit, register, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
     console.log(errors);
     const [val, setVal] = useState("");
 
-    const onChange = (e) => {
-        setVal(e.target.value);
-      };
+    const onChange = (e, field) => {
+        const inputValue = e.target.value;
+        let formattedValue = inputValue;
+
+        if (field === 'cardExpMM') {
+            const numericValue = parseInt(inputValue, 10);
+            if (!isNaN(numericValue) && numericValue >= 1 && numericValue <= 12) {
+                formattedValue = inputValue;
+            }
+        } else {
+            formattedValue = cn_format(inputValue);
+        }
+
+        dispatch(setFormValue(field, formattedValue));
+    };
+
+    useEffect(() => {
+        const formattedValue = cn_format(val);
+        dispatch(setFormValue('cardNumber', formattedValue));
+    }, [val, dispatch]);
+    
+    const validateMonth = (value) => {
+        const numericValue = parseInt(value, 10);
+        return !isNaN(numericValue) && numericValue >= 1 && numericValue <= 12;
+    };
 
     function cn_format(value) {
         const v = value
@@ -33,46 +59,56 @@ function Form() {
                 <div className='form__input1'>
                     <p className='form__input__title'>Cardholder Name</p>
                     <input 
+                    {...register('cardholderName', { required: true, pattern: /[a-zA-Z-]+/ })}
                     className='form__input__cname largeInput' 
                     type="text" 
                     placeholder="e.g. Jane Appleseed" 
                     required
-                    pattern='[a-z A-Z-]+'/>
+                    pattern='[a-z A-Z-]+'
+                    onChange={(e) => onChange(e, 'cardholderName')}/>
                 </div>
                 <div className='form__input2'>
                     <p className='form__input__title'>Card Number</p>
                     <input 
+                    {...register('cardNumber', { required: true })}
                     className='form__input__cnumber largeInput' 
                     type="tel" 
                     placeholder="e.g. 1234 5678 9123 0000" 
                     value={cn_format(val)}
-                    onChange={onChange}
+                    onChange={(e) => setVal(e.target.value)}
                     required />
                 </div>
                 <div className='form__input3'>
                     <p className='form__input__title'>Exp. Date (MM/YY)</p>
                     <div className='form__input__cexpiration--container'>
+                    {errors.cardExpMM && <p className="error-message">Invalid month</p>}
                         <input 
                         className='form__input__cexpiration smallInput' 
                         type="tel" 
                         placeholder="MM"
                         pattern='\d\d'
+                        {...register('cardExpMM', { validate: (value) => validateMonth(value) })}
+                        onChange={(e) => onChange(e, 'cardExpMM')}
                         required />
                         <input 
+                        {...register('cardExpYY', { required: true })}
                         className='form__input__cexpiration smallInput' 
                         type="tel" 
                         placeholder="YY"
                         pattern='\d\d'
+                        onChange={(e) => onChange(e, 'cardExpYY')}
                         required />   
                     </div>
                 </div>
                 <div className='form__input4'>
                     <p className='form__input__title'>CVC</p>
                     <input 
+                    {...register('cardCvc', { required: true, pattern: /\d{3}/ })}
                     className='form__input__cvc smallInput' 
                     type="tel" 
                     placeholder="e.g. 123" 
                     pattern="\d{3}"
+                    onChange={(e) => onChange(e, 'cardCvc')}
                     required />
                 </div>
         
--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,21 +1,32 @@
 import versoCard from "../../assets/bg-card-back.png";
 import rectoCard from "../../assets/bg-card-front.png";
-
 import cardLogo from "../../assets/card-logo.svg";
 
+import { useSelector } from 'react-redux';
+
 function Hero() {
+
+    const formData = useSelector((state) => state);
+    const cardholderName = formData.cardholderName || "Jane Appleseed";
+    const cardNumber = formData.cardNumber || "0000 0000 0000 0000";
+    const cardExpMM = formData.cardExpMM || "00";
+    const cardExpYY = formData.cardExpYY || "00";
+    const cardCvc = formData.cardCvc || "000";
+
+    const formattedExpiration = `${cardExpMM}/${cardExpYY}`;
+
     return(
         <section className="hero">
             <div className="hero__verso">
-                <p className="hero__verso__security">000</p>
+                <p className="hero__verso__security">{cardCvc}</p>
                 <img src={versoCard} alt='verso' className="hero__verso__img"/>
             </div>
             <div className="hero__recto">
                 <img src={cardLogo} alt="card logo" className="hero__recto__cardLogo"/>
-                <p className="hero__recto__number">0000 0000 0000 0000</p>
+                <p className="hero__recto__number">{cardNumber}</p>
                 <div className="hero__recto__infos">
-                    <p className="hero__recto__infos__name">Jane Appleseed</p>
-                    <p className="hero__recto__infos__expiration">00/00</p>
+                    <p className="hero__recto__infos__name">{cardholderName}</p>
+                    <p className="hero__recto__infos__expiration">{formattedExpiration}</p>
                 </div>
                 <img src={rectoCard} alt='recto' className="hero__recto__img"/>
             </div>
